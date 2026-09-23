5222cfd4b54ae55d2f16a2a28454a2d17d200db6
João Carlos de Sousa Santos <joaocarlosdesousa17@gmail.com>
2026-09-23 20:38:14 -0300

    Conclui Degrau 2: criacao, atualizacao e manipulacao de dicionarios

---
+++ degrau2_dicionários.py
@@ -0,0 +1,12 @@
+
+faculdade = {
+
+    "nome" : "João",
+    "curso" : "Ciência da Computação",
+    "nota_poc" : "8,5"
+}
+
+faculdade["semestre"] = 2
+faculdade["nota_poc"] = 10
+
+print(f"\nO aluno {faculdade['nome']} do curso da {faculdade['curso']} teve a nota da POC atualizada para {faculdade['nota_poc']}\n")
